205e797206ab55c6ff6dc1cbde35d08ffd5c9d53
Ubuntu <ubuntu@ip-172-31-3-70.eu-central-1.compute.internal>
2020-10-24 16:04:02 +0000

    change function

--- JSDers1/assets/scripts/app.js
+++ JSDers1/assets/scripts/app.js
@@ -1,8 +1,12 @@
 const defaultResult = 0;
-
 let currentResult = defaultResult;
 
-currentResult = (currentResult + 10)/2;
+function add(num1, num2) {
+    const result = num1 + num2;
+    return result;
+}
+
+currentResult = add(1, 2);
 
 let calculationDescription = `( ${defaultResult} + 10 ) / 2`;
 
