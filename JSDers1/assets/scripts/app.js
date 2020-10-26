f2814f84e8d0b1ca417711151317e1698859d2dd
Ubuntu <ubuntu@ip-172-31-3-70.eu-central-1.compute.internal>
2020-10-26 12:26:12 +0000

    Calculator Last

--- JSDers1/assets/scripts/app.js
+++ JSDers1/assets/scripts/app.js
@@ -1,13 +1,85 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
-function add(num1, num2) {
-    const result = num1 + num2;
-    return result;
+function getUserNumberInput() {
+    return parseInt(userInput.value);
 }
 
-currentResult = add(1, 2);
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription);
+}
 
-let calculationDescription = `( ${defaultResult} + 10 ) / 2`;
+function logger(iOperation, prevResult, iNumber, iResult) {
+    const logEntry = {
+        operation: iOperation,
+        prevResult: prevResult,
+        number: iNumber,
+        result: iResult
+    };
+    logEntries.push(logEntry);
+    console.log(logEntries);
+}
 
-outputResult(currentResult, calculationDescription);
+function calculationResult(calculationType) {
+    const enteredNumber = getUserNumberInput();
+    if (
+        calculationType !== 'ADD'       &&
+        calculationType !== 'SUBSTRACT' &&
+        calculationType !== 'MULTIPLY'  &&
+        calculationType !== 'DIVIDE'    ||
+        !enteredNumber
+    ) {
+        return;
+    }
+    
+    /*if (
+        calculationType === 'ADD'       ||
+        calculationType === 'SUBSTRACT' ||
+        calculationType === 'MULTIPLY'  ||
+        calculationType === 'DIVIDE'
+    ) {*/
+    
+    const initialREsult = currentResult;
+    let mathOperation;
+        
+    if (calculationType === 'ADD') {
+            currentResult += enteredNumber;
+            mathOperation = '+';
+    } else if (calculationType === 'SUBSTRACT') {
+            currentResult -= enteredNumber;
+            mathOperation = '-';
+    } else if(calculationType === 'MULTIPLY') {
+            currentResult *= enteredNumber;
+            mathOperation = '*';
+    } else if(calculationType === 'DIVIDE') {
+            currentResult /= enteredNumber;
+            mathOperation = '/';
+    }
+        
+    createAndWriteOutput(mathOperation, initialREsult, enteredNumber);
+    logger(calculationType, initialREsult, enteredNumber, currentResult);   
+    //}
+}
+
+function add() {
+    calculationResult('ADD');
+}
+
+function subtract() {
+    calculationResult('SUBSTRACT');
+}
+
+function multiply() {
+    calculationResult('MULTIPLY');
+}
+
+function divide() {
+    calculationResult('DIVIDE');
+}
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
